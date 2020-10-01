eaf6b792739857315100d5d355cd36c3cc6684dc
212562268-Cherian,Shejoy <212562268@ge.com>
2020-10-01 11:17:58 +0530

    added posts

---
+++ src/pages/posts/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../../components/layout'
+import Image from '../../components/image'
+import SEO from '../../components/seo'
+
+
+export default () => (
+<Layout>
+    <div>
+        <h1>Hello from posts index 3!!!!!!!!!!!!!!!!!!</h1>
+    </div>
+</Layout>
+)
---
+++ src/pages/posts/page-3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+import Layout from '../../components/layout'
+
+
+
+export default () => (
+  <Layout>
+      <h1>Hello from page 3!!!!!!!!!!!!!!!!!!</h1>
+  </Layout>
+)
